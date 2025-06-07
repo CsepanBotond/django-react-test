44c35e216c802d5b62ae551e575b0d094a88a0da
Csépán Botond <csepan.botond@gmail.com>
2025-06-07 23:05:51 +0200

    feat: add employee CRUD operations

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api.models import Employee
+from api.serializers import EmployeeSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+    def get_queryset(self):
+        queryset = Employee.objects.all()
+        email = self.request.query_params.get('email')
+        name = self.request.query_params.get('name')
+        if email is not None:
+            queryset = queryset.filter(email=email)
+        if name is not None:
+            queryset = queryset.filter(name=name)
+
+        return queryset
